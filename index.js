669ba9176ae5029a88ea7038d734cb8951f36534
Aniket <aniketpratapsingh7310@gmail.com>
2022-06-03 23:44:53 +0530

    :

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT
 const Crouter = require('./Routers/Contactrouter')
 app.use(express.json())
 app.use(Crouter)
+
+
 app.use(Urouter)
 const contact = require('./Models/Contacts')
 app.listen(port, ()=> {
